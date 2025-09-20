b52d1f6620f8e3637ecb6dae4894c707be576395
ItsAndrewC! <andew73885@gmail.com>
2025-09-20 18:15:52 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 # Set the Image name
-IMAGE_NAME = "app"
+IMAGE_NAME="app"
+
+# Create a buildx object 
+
+# Create and use a buildx builder that supports multi-platform
+docker buildx create --name multi-builder --use
+docker buildx inspect --bootstrap
 
 # Run 'docker build' for different hardwares
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -t your-image-name:latest \
+  -t $IMAGE_NAME:latest \
   --push .  # or --load if you want it locally
 
 # Run 'docker run' in VSCode as an integrated terminal, remove once done
-docker run -it -p --rm 8080:80 app
+docker run --rm -p 8080:5000 $IMAGE_NAME:latest
 
