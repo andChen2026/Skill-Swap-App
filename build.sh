5630afaa9aee06c31e85acdf1d7b400257914777
ItsAndrewC! <andew73885@gmail.com>
2025-09-20 20:34:52 -0400

    finished build.sh and run.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+# Set the Image name
+IMAGE_NAME="itsandrewc/app"
+
+# Log in to docker services
+docker login
+
+# Create and use a buildx builder that supports multi-platform
+docker buildx rm multi-builder
+docker buildx create --name multi-builder --use
+docker buildx inspect --bootstrap
+
+# Run 'docker build' for different hardwares
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -t $IMAGE_NAME:latest \
+  --push .  # or --load if you want it locally
--- run.sh
+++ run.sh
@@ -1,18 +1,10 @@
-# Set the Image name
-IMAGE_NAME="app"
-
-# Create a buildx object 
-
-# Create and use a buildx builder that supports multi-platform
-docker buildx create --name multi-builder --use
-docker buildx inspect --bootstrap
-
-# Run 'docker build' for different hardwares
-docker buildx build \
-  --platform linux/amd64,linux/arm64 \
-  -t $IMAGE_NAME:latest \
-  --push .  # or --load if you want it locally
+# Find a dynamic port on the machine
+for port in {5000..5100}; do
+  if ! lsof -iTCP:$port -sTCP:LISTEN >/dev/null; then
+    FREE_PORT=$port
+    break
+  fi
+done
 
 # Run 'docker run' in VSCode as an integrated terminal, remove once done
-docker run --rm -p 8080:5000 $IMAGE_NAME:latest
-
+docker run --rm -p $FREE_PORT:5000 $IMAGE_NAME:latest
